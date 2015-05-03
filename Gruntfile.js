bcb972b20e5a400ced032a1ef2900d678f4ec7be
evilaliv3 <giovanni.pellerano@evilaliv3.org>
2015-05-03 16:14:52 +0200

    Improve UnitTesting by means of Mocha, Istanbul and Coveralls

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,45 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    browserify: {
+      unittests: {
+        files: {
+          'test/lib/unittests-bundle.js': [ './test/unittests.js' ]
+        }
+      }
+    },
+    copy: {
+      test: {
+        expand: true,
+        flatten: true,
+        cwd: 'node_modules/',
+        src: ['mocha/mocha.css', 'mocha/mocha.js', 'chai/chai.js'],
+        dest: 'test/lib/'
+      }
+    },
+    mocha_istanbul: {
+      coverage: {
+        src: 'test'
+      },
+      coveralls: {
+        src: ['test'],
+        options: {
+          coverage:true,
+          reportFormats: ['cobertura','lcovonly']
+        }
+      }
+    },
+    mocha_phantomjs: {
+      all: ['test/**/*.html']
+    },
+    mochaTest: {
+      unittests: {
+        options: {
+          reporter: 'spec'
+        },
+        src: [ 'test/unittests.js' ]
+      }
+    },
     uglify: {
       scrypt: {
         files: {
@@ -10,8 +49,24 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-mocha-istanbul')
+  grunt.loadNpmTasks('grunt-mocha-phantomjs');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.event.on('coverage', function(lcov, done){
+      require('coveralls').handleInput(lcov, function(err){
+          if (err) {
+              return done(err);
+          }
+          done();
+      });
+  });
 
   grunt.registerTask('build', ['uglify']);
-
+  grunt.registerTask('coverage', ['mocha_istanbul:coverage']);
+  grunt.registerTask('coveralls', ['mocha_istanbul:coveralls']);
+  grunt.registerTask('test', ['browserify', 'copy:test', 'mochaTest', 'mocha_phantomjs']);
 };
