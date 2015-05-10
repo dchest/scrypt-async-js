39c5e29eac686d26626087e1f7c0a3d3f13d8848
Dmitry Chestnykh <dmitry@codingrobots.com>
2015-05-10 10:21:08 +0200

    Use protected credentials for SauceLabs.
    Still uses the revealed access key, which needs to be revoked and
    replaced if tests succeed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,8 +47,8 @@ module.exports = function(grunt) {
     'saucelabs-mocha': {
       all: {
         options: {
-          username: 'dchest',
-          key: 'cc31fbb6-680a-4e2f-93f2-0153ffdf03ff',
+          username: process.env.SAUCE_USERNAME,
+          key: process.env.SAUCE_ACCESS_KEY,
           urls: ['http://127.0.0.1:3000/unittests.html'],
           build: process.env.TRAVIS_JOB_ID,
           testname: 'Sauce Unit Test for scrypt-async-js',
