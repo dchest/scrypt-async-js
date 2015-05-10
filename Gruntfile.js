69db1d42da8b971d3a211fa486a0a009b8ae2108
Dmitry Chestnykh <dmitry@codingrobots.com>
2015-05-10 15:40:08 +0200

    Remove repeated coverage test.

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,12 +26,6 @@ module.exports = function(grunt) {
       }
     },
     mocha_istanbul: {
-      coverage: {
-        src: 'test',
-        options: {
-          timeout: 6000,
-        }
-      },
       coveralls: {
         src: ['test'],
         options: {
